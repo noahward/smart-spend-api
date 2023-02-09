65791d0d7bafe16d66745ea6f31a45c675818143
Noah <noahward@shaw.ca>
2023-02-09 09:20:20 -0500

    Create category model and serializer

--- api/apps/category/models.py
+++ api/apps/category/models.py
@@ -1,3 +1,9 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Category(models.model):
+    name = models.CharField(max_length=100, unique=True)
+    detailed_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return str(self.name)
