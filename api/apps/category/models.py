b4bf35ae6d1f03c1f3c3c331167b0bf1173acddd
Noah <noahward@shaw.ca>
2023-02-09 18:59:41 -0500

    Add user_category model to allow users to create local categories

--- api/apps/category/models.py
+++ api/apps/category/models.py
@@ -1,9 +1,36 @@
 from django.db import models
 
+from api.apps.user import User
+
 
 class Category(models.model):
-    name = models.CharField(max_length=100, unique=True)
-    detailed_name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    detailed_name = models.CharField(max_length=100)
 
     def __str__(self):
         return str(self.name)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "detailed_name"], name="unique_category"
+            )
+        ]
+
+
+class UserCategory(models.model):
+    name = models.CharField(max_length=100)
+    detailed_name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_categories"
+    )
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user_id", "name", "detailed_name"], name="unique_user_category"
+            )
+        ]
--- api/apps/category/serializers.py
+++ api/apps/category/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, UserCategory
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["name", "detailed_name"]
+
+
+class UserCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserCategory
+        fields = ["name", "detailed_name", "user_id"]
