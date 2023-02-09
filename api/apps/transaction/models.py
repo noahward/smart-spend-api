c9c5185faa02993ad7346eb69491da074dbcd868
Noah <noahward@shaw.ca>
2023-02-09 09:12:38 -0500

    Create transaction model and serializer

--- api/apps/transaction/models.py
+++ api/apps/transaction/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from api.apps.user import User
+from api.apps.account import Account
+from api.apps.category import Category
+
+
+class Transaction(models.model):
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    currency = models.CharField(max_length=3)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
---
+++ api/apps/transaction/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = [
+            "id",
+            "date",
+            "description",
+            "currency",
+            "amount",
+            "user_id",
+            "account_id",
+            "category_id",
+        ]
