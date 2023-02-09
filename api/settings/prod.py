5dd7ed037fcafc6bbb4d60861b0cd116b4b1106f
Noah <noahward@shaw.ca>
2023-02-08 23:57:32 -0500

    Configure environment variables and django settings

---
+++ api/settings/prod.py
@@ -0,0 +1,31 @@
+import os
+
+import environ
+
+from .base import *
+
+# Environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env.prod"))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "port": env("DB_PORT"),
+    }
+}
