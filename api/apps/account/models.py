eb53de8af7a69a33a1732543284325f1c12082b2
Noah <noahward@shaw.ca>
2023-02-09 19:04:05 -0500

    Add unique constraint to account model

--- api/apps/account/models.py
+++ api/apps/account/models.py
@@ -5,8 +5,13 @@ from api.apps.user import User
 
 class Account(models.model):
     name = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=100)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=100, null=True, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="accounts")
 
     def __str__(self):
-        return str(self.id)
+        return str(self.name)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["name", "user_id"], name="unique_account")
+        ]
