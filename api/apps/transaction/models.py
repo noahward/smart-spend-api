afb731cca0d02f0ef1d635ccc2ff4bca839abb22
Noah <noahward@shaw.ca>
2023-02-09 19:12:41 -0500

    Update field name to be more descriptive

--- api/apps/transaction/models.py
+++ api/apps/transaction/models.py
@@ -8,7 +8,7 @@ from api.apps.category import Category
 class Transaction(models.model):
     date = models.DateField()
     description = models.CharField(max_length=255)
-    currency = models.CharField(max_length=3)
+    currency_code = models.CharField(max_length=3)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
