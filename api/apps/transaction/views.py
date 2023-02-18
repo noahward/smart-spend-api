a716a5dc5c0de5ba0591cf5adb7a071467b980b8
Noah <noahward@shaw.ca>
2023-02-18 13:39:56 -0500

    Refactor transaction retrieval and make migrations

--- api/apps/transaction/views.py
+++ api/apps/transaction/views.py
@@ -1,3 +1,23 @@
-# from django.shortcuts import render
+from rest_framework import generics
+from django.db.models import Q
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from api.apps.account.models import Account
+
+from .models import Transaction
+from .serializers import TransactionSerializer
+
+
+class TransactionList(generics.ListCreateAPIView):
+    model = Transaction
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        account = Account.objects.get(id=self.kwargs.get("aid"))
+        return Transaction.objects.filter(Q(user=user) & Q(account=account))
+
+
+class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
+    pass
