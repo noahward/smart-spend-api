7205983c7759f91bef7633361843c56b927fe75e
Noah <noahward@shaw.ca>
2023-03-12 22:17:34 -0400

    Parse and bulk create transactions from uploaded file

--- api/apps/transaction/serializers.py
+++ api/apps/transaction/serializers.py
@@ -1,9 +1,47 @@
+from django.db import IntegrityError
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import Transaction
 
 
+class ModelObjectIdField(serializers.Field):
+    """
+    We use this when we are doing bulk create/update. Since multiple instances share
+    many of the same fk objects we validate and query the objects first, then modify the request data
+    with the fk objects. This allows us to pass the objects in to be validated.
+    """
+
+    def to_representation(self, value):
+        return value.id
+
+    def to_internal_value(self, data):
+        return data
+
+
+class BulkCreateListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        result = [self.child.create(attrs) for attrs in validated_data]
+
+        try:
+            self.child.Meta.model.objects.bulk_create(result)
+        except IntegrityError as e:
+            raise ValidationError(e)
+
+        return result
+
+
 class TransactionSerializer(serializers.ModelSerializer):
+    account = ModelObjectIdField()
+
+    def create(self, validated_data):
+        instance = Transaction(**validated_data)
+
+        if isinstance(self._kwargs["data"], dict):
+            instance.save()
+
+        return instance
+
     class Meta:
         model = Transaction
         fields = [
@@ -16,3 +54,4 @@ class TransactionSerializer(serializers.ModelSerializer):
             "account",
             "category",
         ]
+        list_serializer_class = BulkCreateListSerializer
--- api/apps/transaction/views.py
+++ api/apps/transaction/views.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from requests import Request
 from rest_framework import generics
 from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
@@ -17,6 +19,30 @@ class TransactionList(generics.ListCreateAPIView):
         user = self.request.user
         account = Account.objects.get(id=self.kwargs.get("aid"))
         return Transaction.objects.filter(Q(user=user) & Q(account=account))
+
+    # TODO: Pydantic for dataframe validation
+    def post(self, request, *args, **kwargs):
+        df = pd.read_csv(
+            request.data["file"],
+            header=None,
+            names=["date", "description", "withdrawal", "deposit", "new_balance"],
+        )
+        df[["withdrawal", "deposit"]] = df[["withdrawal", "deposit"]].fillna(value=0)
+        df["currency_code"] = "CAD"
+        df["account"] = Account.objects.get(id=self.kwargs.get("aid"))
+        df["amount"] = df["deposit"] - df["withdrawal"]
+        df["date"] = pd.to_datetime(df["date"], infer_datetime_format=True)
+        df["user"] = self.request.user
+        df = df.drop(columns=["withdrawal", "deposit", "new_balance"])
+
+        transaction_list = df.to_dict(orient="records")
+        transaction_req = Request(data=transaction_list)
+        return super(TransactionList, self).post(transaction_req, *args, **kwargs)
+
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get("data", {}), list):
+            kwargs["many"] = True
+        return super(TransactionList, self).get_serializer(*args, **kwargs)
 
 
 class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
