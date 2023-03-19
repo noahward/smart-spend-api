54a3f06498bbb7559c33034e2407b86eccd5d99c
Noah <noahward@shaw.ca>
2023-03-19 17:28:48 -0400

    Update timezone

--- api/settings/base.py
+++ api/settings/base.py
@@ -72,7 +72,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = "EST"
 USE_I18N = True
 USE_TZ = True
 
