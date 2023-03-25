7e540b8960d1d99000197337776b6eeba82443ca
Noah <noahward@shaw.ca>
2023-03-24 23:54:57 -0400

    Add unit tests for User views

--- api/apps/user/tests.py
+++ api/apps/user/tests.py
@@ -1,11 +1,13 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 from django.db.utils import IntegrityError
-from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+
+from api.apps.user.models import User
 
 
-class TestUsersManagers(TestCase):
+class UserManagersTests(APITestCase):
     def test_create_user(self):
-        User = get_user_model()
         user = User.objects.create_user(
             email="regular@user.com", password="foo", first_name="bar", last_name="baz"
         )
@@ -20,7 +22,6 @@ class TestUsersManagers(TestCase):
             User.objects.create_user(email="")
 
     def test_create_superuser(self):
-        User = get_user_model()
         admin_user = User.objects.create_superuser(
             email="super@user.com", password="foo", first_name="bar", last_name="baz"
         )
@@ -37,3 +38,58 @@ class TestUsersManagers(TestCase):
                 last_name="baz",
                 is_superuser=False,
             )
+
+
+class UserViewsTests(APITestCase):
+    def test_register(self):
+        url = reverse("register")
+        data = {
+            "email": "register@user.com",
+            "password": "foo",
+            "first_name": "bar",
+            "last_name": "baz",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().email, data["email"])
+        self.assertEqual(response.data["profile"]["email"], data["email"])
+        self.assertIn("key", response.data["token"])
+        self.assertIn("expiry", response.data["token"])
+
+    def test_login_correct_credentials(self):
+        User.objects.create_user(
+            email="login@user.com", password="foo", first_name="bar", last_name="baz"
+        )
+        self.assertTrue(self.client.login(email="login@user.com", password="foo"))
+        url = reverse("login")
+        data = {"email": "login@user.com", "password": "foo"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["profile"]["email"], data["email"])
+        self.assertIn("key", response.data["token"])
+        self.assertIn("expiry", response.data["token"])
+
+    def test_login_bad_credentials(self):
+        User.objects.create_user(
+            email="login@user.com",
+            password="invalid",
+            first_name="bar",
+            last_name="baz",
+        )
+        self.assertTrue(self.client.login(email="login@user.com", password="invalid"))
+        url = reverse("login")
+        data = {"email": "login@user.com", "password": "foo"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["non_field_errors"][0], "Invalid credentials")
+
+    def test_main_user(self):
+        user = User.objects.create_user(
+            email="main@user.com", password="foo", first_name="bar", last_name="baz"
+        )
+        self.client.force_authenticate(user)
+        url = reverse("user")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], "main@user.com")
