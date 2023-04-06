699c01c0f91d56b64bf8bf7711625c74b8fe084c
Noah <noahward@shaw.ca>
2023-04-06 19:39:05 -0400

    Simplify transaction upload view

--- api/apps/transaction/views.py
+++ api/apps/transaction/views.py
@@ -1,16 +1,13 @@
 from datetime import datetime
 
-import pandas as pd
 from requests import Request
-from pandera.errors import SchemaError
 from rest_framework import generics
-from rest_framework.exceptions import APIException
+from django.utils.dateparse import parse_date
 from rest_framework.permissions import IsAuthenticated
 
 from api.apps.user.permissions import IsOwner
 
 from .models import Transaction
-from .df_helpers import validate_df
 from .serializers import TransactionSerializer
 
 
@@ -24,44 +21,16 @@ class TransactionList(generics.ListCreateAPIView):
         return Transaction.objects.filter(user=user)
 
     # TODO: Don't hardcode currency
+    # TODO: Handle file uploads
     def post(self, request, *args, **kwargs):
-        account_id = request.data["account_id"]
-        if "file" in request.data:
-            df = pd.read_csv(request.data["file"], header=None)
+        transaction = request.data
+        transaction["currency_code"] = "CAD"
+        transaction["date"] = parse_date(transaction["date"])
+        transaction["user"] = self.request.user.id
 
-            try:
-                df = df.pipe(validate_df)
-            except (pd.errors.OutOfBoundsDatetime, SchemaError):
-                raise APIException(
-                    "Only unaltered TD statements are accepted at this time"
-                )
-
-            df["currency_code"] = "CAD"
-            df["account"] = account_id
-            df["amount"] = df["deposit"] - df["withdrawal"]
-            df["user"] = self.request.user.id
-            df["date"] = df["date"].dt.date
-            df = df.drop(columns=["withdrawal", "deposit", "new_balance"])
-
-            transaction_list = df.to_dict(orient="records")
-            transaction_req = Request(data=transaction_list)
-
-        else:
-            transaction = request.data
-            transaction["currency_code"] = "CAD"
-            transaction["description"] = "manual_balance_update"
-            transaction["account"] = account_id
-            transaction["date"] = datetime.today().strftime("%Y-%m-%d")
-            transaction["user"] = self.request.user.id
-
-            transaction_req = Request(data=transaction)
+        transaction_req = Request(data=transaction)
 
         return super(TransactionList, self).post(transaction_req, *args, **kwargs)
-
-    def get_serializer(self, *args, **kwargs):
-        if isinstance(kwargs.get("data", {}), list):
-            kwargs["many"] = True
-        return super(TransactionList, self).get_serializer(*args, **kwargs)
 
 
 class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
