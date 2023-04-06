c4f98b1f35d2b12f961ca6e5acfc2621c6ad45fb
Noah <noahward@shaw.ca>
2023-04-06 19:23:15 -0400

    Update model constraints

--- api/apps/category/models.py
+++ api/apps/category/models.py
@@ -5,7 +5,6 @@ from api.apps.user.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    detailed_name = models.CharField(max_length=100)
     user = models.ForeignKey(
         User,
         null=True,
@@ -22,11 +21,6 @@ class Category(models.Model):
         # TODO: Ensure a user can't create a category that an admin has created. E.g. when user=null
         constraints = [
             models.UniqueConstraint(
-                fields=["name", "detailed_name"], name="unique_category"
-            )
-        ]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["user", "name", "detailed_name"], name="unique_user_category"
+                fields=["user", "name"], name="unique_user_category"
             )
         ]
