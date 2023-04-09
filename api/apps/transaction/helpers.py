0e118d98918cf01519aa2ba280cc1dd7feb21ec8
Noah <noahward@shaw.ca>
2023-04-09 18:48:05 -0400

    Helper for parsing ofx files

---
+++ api/apps/transaction/helpers.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+def process_transaction_file(ofx_data, account_map=None, user_id=None):
+    parsed_data = []
+    for account in ofx_data.accounts:
+        transaction_list = account.statement.transactions
+        account_data = {
+            "id": account.account_id,
+            "kind": account.account_type,
+            "transactions": [],
+        }
+
+        for transaction in transaction_list:
+            transaction_data = {
+                "date": datetime.date(transaction.date),
+                "amount": transaction.amount,
+                "description": transaction.payee,
+                "currency_code": account.statement.currency,
+            }
+
+            if user_id:
+                transaction_data["user"] = user_id
+            if account_map:
+                transaction_data["account"] = account_map[account.account_id]
+
+            account_data["transactions"].append(transaction_data)
+        parsed_data.append(account_data)
+
+    return parsed_data
