b7a695c64e14637d9c392200a658f92e9a083cb2
Noah <noahward@shaw.ca>
2023-04-24 21:46:45 -0400

    Abstract transaction URLs to seperate file

--- api/urls.py
+++ api/urls.py
@@ -3,30 +3,14 @@ from django.contrib import admin
 
 from api.apps.user.urls import user_urls
 from api.apps.account.urls import account_urls
-from api.apps.category.views import category_urls
-from api.apps.transaction.views import (
-    TransactionList,
-    TransactionDetail,
-    TransactionFileUpload,
-    preview_transaction_file,
-)
+from api.apps.category.urls import category_urls
+from api.apps.transaction.urls import transaction_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("transactions", TransactionList.as_view(), name="transactions"),
-    path(
-        "transaction-file-preview",
-        preview_transaction_file,
-        name="transaction-file-preview",
-    ),
-    path(
-        "transaction-file-upload",
-        TransactionFileUpload.as_view(),
-        name="transaction-file-upload",
-    ),
-    path("transactions/<int:tid>", TransactionDetail.as_view(), name="transaction"),
 ]
 
 urlpatterns += user_urls
 urlpatterns += account_urls
 urlpatterns += category_urls
+urlpatterns += transaction_urls
